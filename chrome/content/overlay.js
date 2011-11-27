2031f2e1a7171c5a99c6dcf5e91309b97807b8dd
Petro Salema <p.salema@gentics.com>
2011-11-27 11:35:59 +0100

    Adds IO functions for finding extension directory

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -5,6 +5,9 @@
 (function(undefined) {
 'use strict'
 
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+
 var DEBUGGING = true;
 var alohaWindow;
 var composeWindow;
@@ -20,6 +23,94 @@ function error() {
 };
 
 var debug = DEBUGGING ? log : function() {};
+
+
+var XPCOM = {
+
+	/**
+	 * @param {?String} path
+	 * @return {nsILocalFile}
+	 */
+	createFileObj: function(path) {
+		var file = Cc['@mozilla.org/file/local;1']
+					.createInstance(Ci.nsILocalFile);
+		file.initWithPath((path && typeof path === 'string') ? path : 'C:\\');
+		return file;
+	},
+
+	/**
+	 * @param {nsILocalFile}
+	 * @return {Array<String>} names of file contents
+	 */
+	getDirList: function(file) {
+		var entries = file.directoryEntries;
+		var entry;
+		var list = [];
+		while (entries.hasMoreElements()) {
+			try {
+				entry = entries.getNext().QueryInterface(Ci.nsILocalFile);
+				list.push(entry.leafName + (entry.isDirectory() ? '/' : ''));
+			} catch(ex) {
+				error(ex);
+			}
+		}
+		return list;
+	},
+
+	/**
+	 * https://developer.mozilla.org/en/Code_snippets/File_I%2F%2FO
+	 */
+	getSystemDirectory: function(directory) {
+		var dirId,
+		    path;
+
+		switch (directory) {
+		case 'profile':
+			dirId = 'ProfD';
+			break;
+		}
+
+		if (dirId) {
+			path = Cc['@mozilla.org/file/directory_service;1']
+					.getService(Ci.nsIProperties)
+					.get(dirId, Ci.nsIFile)
+					.path;
+		}
+
+		return path ? XPCOM.createFileObj(path) : null;
+	},
+
+	readFile: function(file) {
+		var inputStream = Cc['@mozilla.org/network/file-input-stream;1']
+							.createInstance(Ci.nsIFileInputStream);
+		var sInputStream = Cc['@mozilla.org/scriptableinputstream;1']
+							.createInstance(Ci.nsIScriptableInputStream);
+		inputStream.init(file, 0x01, null, null);
+		sInputStream.init(inputStream);
+		var data = sInputStream.read(sInputStream.available());
+		inputStream.close();
+		sInputStream.close();
+		return data;
+	}
+};
+
+function getExtensionDirectory() {
+	var file = XPCOM.getSystemDirectory('profile');
+	if (!file) {
+		return null;
+	}
+	file.append('extensions');
+	file.append('aloha_with_wings@petrosalema.com');
+	if (file.isDirectory()) {
+		return file;
+	}
+	var path = XPCOM.readFile(file);
+	return path ? XPCOM.createFileObj(path) : '';
+};
+
+var dir = getExtensionDirectory();
+var dirs = XPCOM.getDirList(dir);
+debug('\n' + dirs.join('\n'));
 
 function checkWindows() {
 	if (!composeWindow) {
@@ -72,7 +163,7 @@ function onOverlayLoaded(event) {
 			origSendMsg.apply(this, arguments);
 		};
 	})(GenericSendMessage);
-	
+
 	jQuery(composeWindow).parent().resize(function() {
 		jQuery(alohaWindow).height(jQuery(this).height());
 	});
@@ -140,5 +231,5 @@ window.addEventListener('compose-window-close', onWindowClose, true);
 
 // Intercept message sending
 // https://developer.mozilla.org/en/Extensions/Thunderbird/HowTos/Common_Thunderbird_Use_Cases/Compose_New_Message
-window.addEventListener('compose-send-message', onSendMessage, true);
+// window.addEventListener('compose-send-message', onSendMessage, true);
 })();
